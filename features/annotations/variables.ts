40701d9791952af8d864ceaef13a5a9ad353b48a
keith <kcollins2004@gmail.com>
2022-09-18 18:41:12 -0400

    annotation and inference examples

--- features/annotations/variables.ts
+++ features/annotations/variables.ts
@@ -31,6 +31,38 @@ const logNumber: (i: number) => void = (i: number) => {
   console.log(i);
 };
 
+const logNumber2 = (i: number): void => {
+  console.log(i);
+};
+
 const numberLog = (i) => {
   console.log(i);
 };
+
+// When to use annotations
+// 1) Functions that return the 'any' type
+const json = '{"x":10, "y":20}';
+const coordinates: { x: number; y: number } = JSON.parse(json);
+
+console.log(coordinates);
+
+// When we declare a variable on one line
+// and initialize it later
+let words = ['red', 'green', 'blue'];
+let foundword: boolean;
+
+for (let i = 0; i < words.length; i++) {
+  if (words[i] === 'green') {
+    foundword = true;
+  }
+}
+
+// 3) variable whose type cannot be inferred correctly
+let numbers = [-10, -1, 12];
+let numberAboveZero: boolean | number = false;
+
+for (let i = 0; i < numbers.length; i++) {
+  if (numbers[i] > 0) {
+    numberAboveZero = numbers[i];
+  }
+}
